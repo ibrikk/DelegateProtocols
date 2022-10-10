04ff762831e535fddd292ab272b607de18c5cfa2
hrozenweig <helmut.rozenweig@gmail.com>
2022-10-10 02:41:33 -0400

    Initial Commit

---
+++ DelegateProtocols/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateProtocols
+//
+//  Created by Ibrahim Khalilov on 2022-10-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
